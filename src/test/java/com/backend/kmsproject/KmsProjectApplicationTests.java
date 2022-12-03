db1d43e36da922fe1cb29f45692b6b9a38a3ecbe
ngdangkiet <kietnguyen17052001@gmail.com>
2022-12-03 22:01:44 +0700

    init kms project

---
+++ src/test/java/com/backend/kmsproject/KmsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.kmsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
