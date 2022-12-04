1aa6d5636669f490f4d74b7f41970479c8230cde
ngdangkiet <kietnguyen17052001@gmail.com>
2022-12-04 12:01:20 +0700

    feat: config swagger

---
+++ src/main/java/com/backend/kmsproject/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.backend.kmsproject.config;
+
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+import java.util.Map;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(getComponents())
+                .info(new Info()
+                        .title("KMS OpenAPI document")
+                        .contact(new Contact().name("Nguyen Dang Kiet").email("ndkiet.dev@gmail.com"))
+                        .description("KMS OpenAPI document")
+                        .version("1.0"))
+                .addSecurityItem(new SecurityRequirement().addList("Access Token"));
+    }
+
+    private Components getComponents() {
+        SecurityScheme authorizationHeaderSchema = new SecurityScheme()
+                .name("Authorization")
+                .scheme("bearer")
+                .bearerFormat("jwt")
+                .type(SecurityScheme.Type.HTTP)
+                .in(SecurityScheme.In.HEADER);
+
+        return new Components()
+                .securitySchemes(Map.of("Access Token", authorizationHeaderSchema));
+    }
+}
