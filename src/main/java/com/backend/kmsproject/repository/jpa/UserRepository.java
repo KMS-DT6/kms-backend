84864340fcc23da796e94ceb92473ef3f724749c
ngdangkiet <kietnguyen17052001@gmail.com>
2022-12-05 17:11:34 +0700

    revise

--- src/main/java/com/backend/kmsproject/repository/jpa/UserRepository.java
+++ src/main/java/com/backend/kmsproject/repository/jpa/UserRepository.java
@@ -3,9 +3,11 @@ package com.backend.kmsproject.repository.jpa;
 import com.backend.kmsproject.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     @Query("SELECT u FROM UserEntity u" +
             " LEFT JOIN FETCH u.role r" +
